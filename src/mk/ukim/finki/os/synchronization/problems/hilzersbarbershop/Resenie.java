e66cc8b5b8ec69cea67da383258c63dd04b67c48
ristes <ristes@github.com>
2014-09-25 18:59:40 +0200

    hilzer's barbershop problem

---
+++ src/mk/ukim/finki/os/synchronization/problems/hilzersbarbershop/Resenie.java
@@ -0,0 +1,179 @@
+package mk.ukim.finki.os.synchronization.problems.hilzersbarbershop;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.concurrent.Semaphore;
+import mk.ukim.finki.os.synchronization.*;
+
+/**
+ * 
+ * @author Robert Armenski
+ * @author Nikola Furnadziski
+ * 
+ */
+public class Resenie {
+	static State state;
+
+	// TODO: Definicija na globalni promenlivi i semafori
+
+	// Za musterijata da vleze vo berbernicata i da visecki da ceka red
+	static Semaphore standingRoom;
+	// Za musterijata da sedne na sofata
+	static Semaphore sofa;
+	// Za musterijata da sedne na berberskiot stol
+	static Semaphore chair;
+	// Za berberot da kaze deka e sloboden da go potstrize musterijata
+	static Semaphore barber;
+	// Za musterijata da oznaci deka sedi na stolot i saka da bide potstrizan
+	static Semaphore customer;
+	// Za musterijata da oznaci deka e spremen za potstrizuvanje
+	static Semaphore readyForHaircut;
+	// Za berberot da kaze koga e gotovo potstrizuvanjeto
+	static Semaphore hairDone;
+	// Za musterijata da gi dade parite za potstrizuvanje
+	static Semaphore cash;
+	// Za berberot da ja dade fiskalnata smetka
+	static Semaphore receipt;
+
+	public static void init(int numBarbers) {
+		// Bidejki vkupno mora da ima ne poveke od 20 musterii vnatre
+		// A 4 mesta se rezervirani za sofata
+		// Ostanuvaat 16 mesta, od koi numBarbers se rezervirani za stolovite
+		// Zatoa moze da ima maksimalno 15 barberi (t.e. stolovi)
+		standingRoom = new Semaphore(16 - numBarbers);
+
+		// 4 mesta rezervirani za sofata
+		sofa = new Semaphore(4);
+		// Po eden stol za sekoj berber
+		chair = new Semaphore(numBarbers);
+		// Inicijalno nema spremni berberi
+		barber = new Semaphore(0);
+		// Inicijalno nema pristignati musterii koi sedat na stol
+		customer = new Semaphore(0);
+		// Inicijalno nema spremni musterii
+		readyForHaircut = new Semaphore(0);
+		// Inicijalno nema potstrizani musterii
+		hairDone = new Semaphore(0);
+		// Inicijalno nikoj ne dal pari
+		cash = new Semaphore(0);
+		// Inicijalno ne se izdadeni fiskalni smetki
+		receipt = new Semaphore(0);
+	}
+
+	public static class Barber extends TemplateThread {
+		public int barberId;
+
+		public Barber(int numRuns, int id) {
+			super(numRuns);
+			barberId = id;
+		}
+
+		@Override
+		public void execute() throws InterruptedException {
+			// Ceka musterija da sedne na stol
+			customer.acquire();
+			// Kazuva deka e sloboden za da go potstrize
+			barber.release();
+			// Ceka musterijata da bide spremen za potstrizuvanje
+			readyForHaircut.acquire();
+			// Go potstrizuva musterijata
+			state.cutHair();
+			// Signalizira deka musterijata e potstrizan
+			hairDone.release();
+
+			// Ceka musterijata da plati
+			cash.acquire();
+			// Gi prifaka parite (i ja pecati fiskalnata)
+			state.acceptPayment();
+			// Mu ja dava fiskalnata smetka na musterijata
+			receipt.release();
+		}
+	}
+
+	public static class Customer extends TemplateThread {
+		public int custId;
+
+		public Customer(int numRuns, int cId) {
+			super(numRuns);
+			this.custId = cId;
+		}
+
+		@Override
+		public void execute() throws InterruptedException {
+			// Ogranicuva max. 20 musterii da bidat prisutni vnatre
+			standingRoom.acquire();
+			// Otkako se oslobodilo mesto za visenje, vleguva vo prodavnicata
+			state.enterShop();
+
+			// Ceka red za sedenje na sofata
+			sofa.acquire();
+			// Sednuva na sofata
+			state.sitOnSofa();
+			// Osloboduva edno mesto za visenje
+			standingRoom.release();
+
+			// Ceka red za sedenje na stolot
+			chair.acquire();
+			// Sednuva na berberskiot stol
+			state.sitInBarberChair();
+			// Osloboduva edno mesto za sedenje na sofata
+			sofa.release();
+
+			// Kazuva deka e pristignat i sedi na stolot
+			customer.release();
+			// Ceka da se oslobodi berber
+			barber.acquire();
+			// Signalizira deka saka da bide potstrizan
+			state.getHairCut();
+			// Kakuva deka e spremen za potstrizuvanje
+			readyForHaircut.release();
+
+			// Ceka da bide potstrizan
+			hairDone.acquire();
+			// Signalizira deka e spremen da plati za uslugata
+			state.pay();
+			// Gi dava parite
+			cash.release();
+			// Ceka fiskalna smetka
+			receipt.acquire();
+
+			// Trgnuva kon izlezot
+			state.exitShop();
+			// Kazuva deka ima sloboden stol
+			chair.release();
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 50; i++) {
+			System.out.print((i + 1) + ": ");
+			run();
+		}
+	}
+
+	public static void run() {
+		try {
+			int numBarbers = 5; // Max: 15
+			int numCustomers = 10 * numBarbers;
+
+			state = new State(numBarbers, numCustomers);
+			HashSet<Thread> threads = new HashSet<Thread>();
+
+			for (int i = 0; i < numBarbers; i++) {
+				Barber b = new Barber(Math.max(numCustomers / numBarbers, 1), i);
+				threads.add(b);
+			}
+			for (int i = 0; i < numCustomers; i++) {
+				Customer cust = new Customer(1, i);
+				threads.add(cust);
+			}
+
+			init(numBarbers);
+
+			ProblemExecution.start(threads, state);
+			System.out.println(new Date().getTime());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ src/mk/ukim/finki/os/synchronization/problems/hilzersbarbershop/State.java
@@ -0,0 +1,192 @@
+package mk.ukim.finki.os.synchronization.problems.hilzersbarbershop;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import mk.ukim.finki.os.synchronization.*;
+
+/**
+ * 
+ * @author Robert Armenski
+ * @author Nikola Furnadziski
+ * 
+ */
+public class State extends AbstractState {
+	@SuppressWarnings("unused")
+	private final int numBarbers, numCustomers;
+
+	private BoundCounterWithRaceConditionCheck arrivedCount;
+	private HashSet<Integer> arrivedCustomers;
+
+	private BoundCounterWithRaceConditionCheck sofaCount;
+	private HashSet<Integer> sofaCustomers;
+
+	private BoundCounterWithRaceConditionCheck chairCount;
+	private HashSet<Integer> chairCustomers;
+	private HashSet<Integer> waitingCustomers;
+
+	private BoundCounterWithRaceConditionCheck toPayCount;
+	private BoundCounterWithRaceConditionCheck cuttedCount;
+	private BoundCounterWithRaceConditionCheck payedCount;
+
+	public State(int numBarbers, int numCust) {
+		super();
+		this.numBarbers = numBarbers;
+		this.numCustomers = numCust;
+
+		arrivedCount = new BoundCounterWithRaceConditionCheck(0, 20, 5,
+				"More than 20 customers cannot be inside at the same time.", 0,
+				5, "There cannot be less than 0 customers in the store");
+
+		sofaCount = new BoundCounterWithRaceConditionCheck(
+				0,
+				4,
+				5,
+				"More than 4 customers cannot sit on the sofa at the same time",
+				0, 5, "There cannot be less than 0 customers on the sofa");
+
+		chairCount = new BoundCounterWithRaceConditionCheck(0, numBarbers, 5,
+				"More than  " + numBarbers
+						+ " customers cannot get a haircut at the same time",
+				0, 5, "There cannot be less than 0 customers on the chairs");
+
+		toPayCount = new BoundCounterWithRaceConditionCheck(0, numCustomers, 5,
+				"More than " + numCustomers + " customers want to pay", 0, 5,
+				"Less than 0 customers want to pay");
+
+		cuttedCount = new BoundCounterWithRaceConditionCheck(0, numCustomers,
+				5, "More than " + numCustomers + " customers had a haircut", 0,
+				5, "Cutted less than 0 customers");
+
+		payedCount = new BoundCounterWithRaceConditionCheck(0, numCustomers, 5,
+				"More than " + numCustomers + " customers payed for a haircut",
+				0, 5, "Less than 0 customers payed for a haircut");
+
+		arrivedCustomers = new HashSet<Integer>();
+		sofaCustomers = new HashSet<Integer>(4);
+		chairCustomers = new HashSet<Integer>(numBarbers);
+		waitingCustomers = new HashSet<Integer>();
+	}
+
+	@Override
+	public synchronized void finalize() {
+		// TODO: final step of state
+		if (arrivedCount.getValue() != 0) {
+			log(new PointsException(10, "There are still "
+					+ arrivedCount.getValue() + " customers inside the store"),
+					null);
+		}
+		if (payedCount.getValue() != cuttedCount.getValue()) {
+			log(new PointsException(10, payedCount.getValue()
+					+ " customers payed but " + cuttedCount.getValue()
+					+ " were cutted"), null);
+		}
+	}
+
+	/*
+	 * Customer methods
+	 */
+	public void enterShop() {
+		synchronized (this) {
+			Resenie.Customer c = (Resenie.Customer) Thread.currentThread();
+			if (arrivedCustomers.contains(c.custId)) {
+				log(new PointsException(5, "The customer has already arrived"),
+						null);
+			} else {
+				arrivedCustomers.add(c.custId);
+				log(null, "A customer enters the shop");
+				Switcher.forceSwitch(10);
+				log(arrivedCount.incrementWithMax(false), null);
+			}
+		}
+	}
+
+	public void sitOnSofa() {
+		synchronized (this) {
+			Resenie.Customer c = (Resenie.Customer) Thread.currentThread();
+			if (sofaCustomers.contains(c.custId)) {
+				log(new PointsException(5,
+						"The customer is already seated on the sofa"), null);
+			} else {
+				sofaCustomers.add(c.custId);
+				log(null, "A customer sits on the sofa");
+				log(sofaCount.incrementWithMax(false), null);
+			}
+		}
+	}
+
+	public synchronized void sitInBarberChair() {
+		synchronized (this) {
+			Resenie.Customer c = (Resenie.Customer) Thread.currentThread();
+			if (chairCustomers.contains(c.custId)) {
+				log(new PointsException(5,
+						"The customer is already seated in the chair"), null);
+			} else {
+				Switcher.forceSwitch(10);
+				chairCustomers.add(c.custId);
+				sofaCustomers.remove(c.custId);
+				log(null, "A customer sits in the chair");
+				log(sofaCount.decrementWithMin(false), null);
+				// Switcher.forceSwitch(10);
+				log(chairCount.incrementWithMax(false), null);
+			}
+		}
+	}
+
+	public synchronized void getHairCut() {
+		Resenie.Customer c = (Resenie.Customer) Thread.currentThread();
+		if (!chairCustomers.contains(c.custId)) {
+			log(new PointsException(5, "The customer " + c.custId
+					+ " is not seated in the chair"), null);
+		} else {
+			if (waitingCustomers.contains(c.custId)) {
+				log(new PointsException(5, "The customer " + c.custId
+						+ " is already waiting"), null);
+			} else {
+				Switcher.forceSwitch(10);
+				waitingCustomers.add(c.custId);
+				log(null, "A customer waits for haircut");
+			}
+		}
+	}
+
+	public void pay() {
+		log(null, "A customer is waiting to pay");
+		Switcher.forceSwitch(10);
+		log(toPayCount.incrementWithMax(false), null);
+	}
+
+	public synchronized void exitShop() {
+		Resenie.Customer c = (Resenie.Customer) Thread.currentThread();
+		chairCustomers.remove(c.custId);
+		arrivedCustomers.remove(c.custId);
+		log(null, "A customer is leaving the shop");
+		Switcher.forceSwitch(10);
+		log(arrivedCount.decrementWithMin(false), null);
+		Switcher.forceSwitch(10);
+		log(chairCount.decrementWithMin(false), null);
+	}
+
+	/*
+	 * Barber methods
+	 */
+	public synchronized void cutHair() {
+		Iterator<Integer> iter = waitingCustomers.iterator();
+		if (!iter.hasNext()) {
+			log(new PointsException(5, "There is no customer to cut their hair"),
+					null);
+		} else {
+			int cId = iter.next();
+			waitingCustomers.remove(cId);
+			log(null, "A customer is getting his haircut");
+			log(cuttedCount.incrementWithMax(false), null);
+		}
+	}
+
+	public void acceptPayment() {
+		log(null, "A customer has payed");
+		Switcher.forceSwitch(10);
+		log(toPayCount.decrementWithMin(false), null);
+		Switcher.forceSwitch(10);
+		log(payedCount.incrementWithMax(false), null);
+	}
+}
